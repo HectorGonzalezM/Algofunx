4a8dda244dd62e21982beb9d119c7a74ffcf5460
Héctor González <92290861+HectorGonzalezM@users.noreply.github.com>
2022-03-12 13:43:15 -0600

    Update arrays.c

--- Unit1/arrays.c
+++ Unit1/arrays.c
@@ -3,9 +3,7 @@
 int main(){
     int myArray[3]={5, 2, 3};
     int i = 0;
-
-    printf("[");
-    //Void//
+    
     void printArray(int myArray[3], int arrayLength){
         while(i < 3){        
             printf("%d", myArray[i]);
@@ -15,9 +13,6 @@ int main(){
             i++;
         }
     }
-    //Print//
     printArray(myArray, 3);
-    printf("]\n");
-
     return 0;
 }
